cae950d284002872d19a38399540a3ed4abaf96e
Tom Peres <tomp@wix.com>
2019-08-04 10:51:13 +0300

    greynode fixes

--- sled-service/index.js
+++ sled-service/index.js
@@ -1,8 +1,7 @@
 const bootstrap = require('@wix/wix-bootstrap-ng');
-const CREATE_TOPIC = require('./lib/topics').GREETING;
 
 bootstrap()
-  .use(require('@wix/wix-bootstrap-greynode'), {createTopics: [{name: CREATE_TOPIC}]})
+  .use(require('@wix/wix-bootstrap-greynode'))
   .config('./lib/config')
   .express('./lib/express')
   .start();
