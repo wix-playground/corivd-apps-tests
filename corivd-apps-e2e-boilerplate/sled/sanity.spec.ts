c919ae89016d45fe0dd89e270f67b237bab764d5
Nir Naor <nirnaori@gmail.com>
2019-08-08 10:27:40 +0300

    Update sanity.spec.ts

--- corivd-apps-e2e-boilerplate/sled/sanity.spec.ts
+++ corivd-apps-e2e-boilerplate/sled/sanity.spec.ts
@@ -1,6 +1,7 @@
 /// <reference types="sled-test-runner" />
 import { Page } from 'puppeteer';
 import { withViewer } from 'santa-integration-tests'
+import testUrl from 'my-test-url-service';
 
 describe('wix.com sanity', () => {
 
@@ -8,17 +9,12 @@ describe('wix.com sanity', () => {
 
   it('CFA Button with vanilla sled', async () => {
     const { page }: { page: Page } = await sled.newPage({});
-    await page.goto('https://www.wix.com');
+    const rcUrl = await testUrl.get('https://www.wix.com');
+    await page.goto(rcUrl);
     const slctr = `#${cfaButtonId}`;
     await page.waitForSelector(slctr);
     const text = await page.$eval(slctr, (e: HTMLElement) => e.innerText);
     expect(text).toBe('Get Started');
   });
-
-  it('CFA Button with santa-integration-tests', withViewer({baseUrl: 'https://www.wix.com'}, async ({componentDriverFactory}) => {
-      const buttonDriver = await componentDriverFactory.create(cfaButtonId) // https://github.com/wix-private/santa-integration-tests#viewersitebuttoncomponent
-      const btnText = await buttonDriver.getText()
-      expect(btnText).toEqual('Get Started')
-  }))
   
 });
