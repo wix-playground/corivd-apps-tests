0c51398e48ce2dbcafaac96990eb930f75827dd8
Tom Peres <tomp@wix.com>
2019-08-09 12:11:10 +0300

    fixed wix.com sanity test to be stable

--- corivd-apps-e2e-boilerplate/sled/sanity.spec.ts
+++ corivd-apps-e2e-boilerplate/sled/sanity.spec.ts
@@ -3,15 +3,15 @@ import { Page } from 'puppeteer';
 
 describe('wix.com sanity', () => {
 
-  const cfaButtonId = 'comp-jx8mrtll';
+  const cfaButtonId = 'comp-jx8pl0hy';
 
-  it('CFA Button with vanilla sled', async () => {
+  it(`'Sign In' Button with vanilla sled`, async () => {
     const { page }: { page: Page } = await sled.newPage({});
     await page.goto('https://www.wix.com');
     const slctr = `#${cfaButtonId}`;
     await page.waitForSelector(slctr);
     const text = await page.$eval(slctr, (e: HTMLElement) => e.innerText);
-    expect(text).toBe('Get Started');
+    expect(text).toBe('Sign In');
   });
   
 });
