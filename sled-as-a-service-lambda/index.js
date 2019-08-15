19385e0819e133de10669eefed6ea794e72e1718
Nir Naor <nirnaori@gmail.com>
2019-08-15 11:03:11 +0300

    Triggering replay using an action specified from the client

--- sled-as-a-service-lambda/index.js
+++ sled-as-a-service-lambda/index.js
@@ -1,3 +1,12 @@
 const { replay } = require('sled-test-runner/dist/src/cli')
 
-exports.handler = async ({ artifact }) => replay({ artifact });
+const replayForArtifact = event => replay({ artifact: event.artifact });
+
+const actions = {
+    replayForArtifact
+}
+
+exports.handler = async event => {
+    const action = event.action || 'replayForArtifact'
+    return actions[action](event)
+} 
